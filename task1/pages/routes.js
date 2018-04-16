69eab598beb0fb69f542762b2c420b902caba1ef
maksimn <maksim1989sovunion@yandex.ru>
2018-04-16 04:16:45 +0300

    Events query is working now

--- task1/pages/routes.js
+++ task1/pages/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
-const { index } = require('./controllers');
 
-router.get('/', index);
+router.get('/', (req, res) => {
+    res.send(`
+        <html>
+        <head>
+            <title>Shri 2018</title>
+        </head>
+        <h1>Hello</h1>
+        </html>
+    `);
+});
 
 module.exports = router;
