f2be48e77cdaf095563ca590ec079bb204d122ab
maksimn <maksim1989sovunion@yandex.ru>
2018-04-21 01:28:06 +0300

    HTML markup to index.html

--- task1/pages/routes.js
+++ task1/pages/routes.js
@@ -3,14 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`
-        <html>
-        <head>
-            <title>Shri 2018</title>
-        </head>
-        <h1>Hello</h1>
-        </html>
-    `);
+    res.sendFile('./index.html',  { root: __dirname });
 });
 
 module.exports = router;
