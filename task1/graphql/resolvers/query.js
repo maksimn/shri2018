42189cf38d214623c00e41b24345db1908306da7
maksimn <maksim1989sovunion@yandex.ru>
2018-04-20 20:43:26 +0300

    events query

--- task1/graphql/resolvers/query.js
+++ task1/graphql/resolvers/query.js
@@ -1,32 +1,39 @@
 const { models } = require('../../models');
 
+const eventFindOptions = {
+  include: [
+    { model: models.Room },
+    { model: models.User }
+  ]
+};
+
+const setEventDataValues = event => {
+  event.dataValues.room = event.Room.dataValues;
+  event.dataValues.users = event.Users;
+
+  return event.dataValues;
+};
+
 module.exports = {
   event (root, { id }) {
-    return models.Event.findById(id, {
-      include: [
-        { model: models.Room },
-        { model: models.User }
-      ]
-    }).then(event => {
-      event.dataValues.room = event.Room.dataValues;
-      event.dataValues.users = event.Users;
-
-      return event.dataValues;
-    });
+    return models.Event.findById(id, eventFindOptions)
+        .then(setEventDataValues);
   },
   events (root, args, context) {
-    return models.Event.findAll({}, context);
+    return models.Event.findAll(eventFindOptions).then(events => {
+      return events.map(setEventDataValues);
+    });
   },
   user (root, { id }) {
     return models.User.findById(id);
   },
   users (root, args, context) {
-    return models.User.findAll({}, context);
+    return models.User.findAll();
   },
   room (root, { id }) {
     return models.Room.findById(id);
   },
   rooms (root, args, context) {
-    return models.Room.findAll({}, context);
+    return models.Room.findAll();
   }
 };
